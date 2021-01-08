dbaac4944fead7b1b2b6e93f01b0aa9fad1e0c5b
gs-maker <gerald.ssali@gmail.com>
2021-01-08 22:33:53 +0300

    revised progress bar

--- 07 Custom Progress Bar/index.js
+++ 07 Custom Progress Bar/index.js
@@ -1,5 +1,6 @@
 const progress = document.querySelector(".progress-done");
 
+// set time out
 setTimeout(() => {
 	progress.style.opacity = 1;
 	progress.style.width = progress.getAttribute("data-done") + "%";
