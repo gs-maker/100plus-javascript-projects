3be061ea58f9c033a812129076e9f1b540e6a44c
gs-maker <gerald.ssali@gmail.com>
2021-01-12 05:57:25 +0300

    pause functionality yet to be fixed

--- 11 Pomodoro Timer/index.js
+++ 11 Pomodoro Timer/index.js
@@ -1,6 +1,6 @@
 let start = document.getElementById("start");
 let reset = document.getElementById("reset");
-let pause = document.getElementById("stop");
+let stop = document.getElementById("stop");
 
 let wm = document.getElementById("workMinutes");
 let ws = document.getElementById("workSeconds");
@@ -8,25 +8,19 @@ let ws = document.getElementById("workSeconds");
 let bm = document.getElementById("breakMinutes");
 let bs = document.getElementById("breakSeconds");
 
-// reference to timer variable
-let startTimer;
-let timeRunning = false;
+// variable reference for start time
+let startTime;
+let timerRunning = false;
 
 // start timer
 start.addEventListener("click", function () {
-	if (startTimer === undefined) {
-		setTimer = setInterval(timer, 1000);
-		timeRunning = true;
+	if (startTime == undefined) {
+		setInterval(timer, 1000);
+		timerRunning = true;
 	}
 });
 
-// stop timer
-pause.addEventListener("click", function () {
-	stopInterval();
-	startTimer = undefined;
-});
-
-//reset timer
+// reset timer
 reset.addEventListener("click", function () {
 	wm.innerText = 25;
 	ws.innerText = "00";
@@ -36,10 +30,15 @@ reset.addEventListener("click", function () {
 
 	document.getElementById("counter").innerText = 0;
 	stopInterval();
-	startTimer = undefined;
+	timerRunning = false;
 });
 
-// timer function
+// stop timer
+stop.addEventListener("click", function () {
+	stopInterval();
+	timerRunning = false;
+});
+
 function timer() {
 	// work timer
 	if (ws.innerText != 0) {
@@ -59,7 +58,7 @@ function timer() {
 		}
 	}
 
-	// reset timer
+	// increment by one when work timer and break timer are zero
 	if (wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0) {
 		wm.innerText = 25;
 		ws.innerText = "00";
@@ -72,5 +71,5 @@ function timer() {
 }
 
 function stopInterval() {
-	clearInterval(startTimer);
+	clearInterval(startTime);
 }
