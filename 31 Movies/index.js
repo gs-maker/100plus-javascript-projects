d1f30db38d9c7a8a2a33475d91fceaa7b7334a96
gs-maker <gerald.ssali@gmail.com>
2021-01-27 21:16:26 +0300

    rating added

--- 31 Movies/index.js
+++ 31 Movies/index.js
@@ -35,7 +35,7 @@ function showMovies(movies) {
         <img src="${IMG_PATH + poster_path}" alt="${title}">
         <div class="movie-info">
             <h3>${title}</h3>
-            <span class="green">${vote_average}</span>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
         </div>
         <div class="overview">
             <h3>Overview</h3>
@@ -44,4 +44,15 @@ function showMovies(movies) {
         `;
 		main.appendChild(movieEl);
 	});
+}
+
+// color coded rating
+function getClassByRate(vote) {
+	if (vote >= 8) {
+		return "green";
+	} else if (vote >= 5) {
+		return "orange";
+	} else {
+		return "red";
+	}
 }
