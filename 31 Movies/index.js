6be4780a4898c944a8a850ce6978212e2b3a57c9
gs-maker <gerald.ssali@gmail.com>
2021-01-28 22:02:47 +0300

    revised call

--- 31 Movies/index.js
+++ 31 Movies/index.js
@@ -7,61 +7,90 @@ const form = document.getElementById("form");
 const search = document.getElementById("search");
 const main = document.getElementById("main");
 
-getMovies(API_URL);
-
-function getMovies(url) {
+getMovies();
+function getMovies(API_URL) {
 	fetch(url)
 		.then((res) => res.json())
-		.then((data) => {
-			data.results;
-			showMovies(data.results);
-		})
-		.catch((error) => console.log("error: ", error));
+		.then((data) => data.results)
+		.catch((error) => console.log("error:", error));
+
+
+		
 }
 
-// function to display movies
-function showMovies(movies) {
-	main.innerHTML = "";
-	movies.map((movie) => {
-		const { title, poster_path, vote_average, overview } = movie;
 
-		const movieEl = document.createElement("div");
-		movieEl.classList.add("movie");
 
-		movieEl.innerHTML = `
-        <img src="${IMG_PATH + poster_path}" alt="${title}">
-        <div class="movie-info">
-            <h3>${title}</h3>
-            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-        </div>
-        <div class="overview">
-            <h3>Overview</h3>
-            ${overview}
-        </div>
-        `;
-		main.appendChild(movieEl);
-	});
-}
 
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	const searchTerm = search.value;
-	if (searchTerm && searchTerm !== "") {
-		getMovies(SEARCH_API + searchTerm);
 
-		search.value = "";
-	} else {
-		window.location.reload();
-	}
-});
 
-// color coded rating
-function getClassByRate(vote) {
-	if (vote >= 8) {
-		return "green";
-	} else if (vote >= 5) {
-		return "orange";
-	} else {
-		return "red";
-	}
-}
+
+
+
+// getMovies()
+// function getMovies(){
+// 	fetch(API_URL)
+// 	.then((res)=>res.json())
+// 	.then((data)=>{
+// 		data.results
+// 		showMovies(data.results)
+// 	})
+// }
+
+// getMovies(API_URL);
+
+// function getMovies(url) {
+// 	fetch(url)
+// 		.then((res) => res.json())
+// 		.then((data) => {
+// 			data.results;
+// 			showMovies(data.results);
+// 		})
+// 		.catch((error) => console.log("error: ", error));
+// }
+
+// // function to display movies
+// function showMovies(movies) {
+// 	main.innerHTML = "";
+// 	movies.map((movie) => {
+// 		const { title, poster_path, vote_average, overview } = movie;
+
+// 		const movieEl = document.createElement("div");
+// 		movieEl.classList.add("movie");
+
+// 		movieEl.innerHTML = `
+//         <img src="${IMG_PATH + poster_path}" alt="${title}">
+//         <div class="movie-info">
+//             <h3>${title}</h3>
+//             <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+//         </div>
+//         <div class="overview">
+//             <h3>Overview</h3>
+//             ${overview}
+//         </div>
+//         `;
+// 		main.appendChild(movieEl);
+// 	});
+// }
+
+// form.addEventListener("submit", (e) => {
+// 	e.preventDefault();
+// 	const searchTerm = search.value;
+// 	if (searchTerm && searchTerm !== "") {
+// 		getMovies(SEARCH_API + searchTerm);
+
+// 		search.value = "";
+// 	} else {
+// 		window.location.reload();
+// 	}
+// });
+
+// // color coded rating
+// function getClassByRate(vote) {
+// 	if (vote >= 8) {
+// 		return "green";
+// 	} else if (vote >= 5) {
+// 		return "orange";
+// 	} else {
+// 		return "red";
+// 	}
+// }
