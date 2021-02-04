b1982bc9f49a31c596e7acbc440e08a71624502d
gs-maker <gerald.ssali@gmail.com>
2021-02-04 23:09:35 +0300

    css done

--- 34 Pexels/index.js
+++ 34 Pexels/index.js
@@ -2,7 +2,7 @@ const API_key = "563492ad6f917000010000019e38357ea9604a3d98fb3b29bbef78d3";
 const url = "https://api.pexels.com/v1/curated?per_page=20&page=1";
 
 // selection from DOM
-const searchInput = document.querySelector("#search");
+const searchInput = document.querySelector(".search");
 const searchBtn = document.querySelector(".search-btn");
 const gallery = document.querySelector(".gallery");
 const form = document.querySelector(".form");
@@ -11,8 +11,14 @@ let search;
 // value for search field
 searchInput.addEventListener("input", inputUpdate);
 function inputUpdate(e) {
-	searchInput = e.target.value;
+	search = e.target.value;
 }
+
+// form functionality
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	searchPhotos(search);
+});
 
 // fetch  API
 async function fetchAPI(url) {
@@ -29,14 +35,14 @@ async function fetchAPI(url) {
 
 // curated photos
 async function curatedPhotos() {
-	const data = await fetchAPI("https://api.pexels.com/v1/curated?&per_page=15&page=1");
+	const data = await fetchAPI("https://api.pexels.com/v1/curated?&per_page=20&page=1");
 	generatePictures(data);
 }
 
 // search particular photos
 async function searchPhotos(query) {
 	clearGallery();
-	const data = await fetchAPI(`https://api.pexels.com/v1/search?query=${query}&per_page=1`);
+	const data = await fetchAPI(`https://api.pexels.com/v1/search?query=${query}&per_page=20`);
 	generatePictures(data);
 }
 
@@ -48,9 +54,12 @@ function generatePictures(data) {
 
 		// photo element
 		galleryImg.innerHTML = `
-            <img src=${photo.src.medium} alt=${photo.photographer}/>
-            <p>${photo.photographer}</p>
-            <a href="${photo.photographer_url}" target="_blank"> </a>
+            <img src=${photo.src.large}  alt=${photo.photographer}/>
+            <p class="name">${photo.photographer}</p>
+            <div class="photo-info">
+                <a href=${photo.photographer_url} target="_blank" class="name">More photos</a>
+                <a href=${photo.src.original} target="_blank" class="download">Download</a>
+            </div>
         `;
 		gallery.appendChild(galleryImg);
 	});
@@ -59,6 +68,7 @@ function generatePictures(data) {
 // clear gallery on search
 function clearGallery() {
 	gallery.innerHTML = "";
+	searchInput.value = "";
 }
 
 curatedPhotos();
