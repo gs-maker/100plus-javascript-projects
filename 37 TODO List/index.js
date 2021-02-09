c86ed5e3f0663e7e0577ed9d3a228a6264c5dd3c
gs-maker <gerald.ssali@gmail.com>
2021-02-09 18:29:17 +0300

    infinite scroll project

--- 37 TODO List/index.js
+++ 37 TODO List/index.js
@@ -1,40 +1,90 @@
 // selectors
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-btn");
-const todoList = document.querySelector(".todo-container");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector("#filter-todo");
 
 // event listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 // functions
-function addTodo(e) {
-	e.preventDefault();
-
-	// create a div for todo item
+function addTodo(event) {
+	// prevent form from submitting
+	event.preventDefault();
+	// todo div
 	const todoDiv = document.createElement("div");
 	todoDiv.classList.add("todo");
 
-	// create new todo item
+	//create li element
 	const newTodo = document.createElement("li");
 	newTodo.innerText = todoInput.value;
 	newTodo.classList.add("todo-item");
 	todoDiv.appendChild(newTodo);
 
-	// add completed button
+	//check-mark button
 	const completeBtn = document.createElement("button");
-	completeBtn.classList.add("complete-btn");
 	completeBtn.innerHTML = `<ion-icon name="checkbox-outline"></ion-icon>`;
-	newTodo.appendChild(completeBtn);
+	completeBtn.classList.add("complete-btn");
+	todoDiv.appendChild(completeBtn);
 
-	// add delete button
-	const deleteBtn = document.createElement("button");
-	deleteBtn.classList.add("trash-btn");
-	deleteBtn.innerHTML = `<ion-icon name="trash-outline"></ion-icon>`;
-	newTodo.appendChild(deleteBtn);
+	//check-mark button
+	const trashBtn = document.createElement("button");
+	trashBtn.innerHTML = `<ion-icon name="trash-outline"></ion-icon>`;
+	trashBtn.classList.add("trash-btn");
+	todoDiv.appendChild(trashBtn);
 
-	// add new item to container
+	// append to list
 	todoList.appendChild(todoDiv);
 
-	// clear input field after entering item
+	// clear todo input value
 	todoInput.value = "";
+}
+
+// delete check function
+function deleteCheck(e) {
+	const item = e.target;
+
+	// delete
+	if (item.classList[0] === "trash-btn") {
+		const todo = item.parentElement;
+		// animation
+		todo.classList.add("slide");
+		todo.addEventListener("transitionend", () => {
+			todo.remove();
+		});
+	}
+
+	// checkmark
+	if (item.classList[0] === "complete-btn") {
+		const todo = item.parentElement;
+		todo.classList.toggle("completed");
+	}
+}
+
+// function to filter todos
+function filterTodo(e) {
+	const todos = todoList.childNodes;
+	todos.forEach((todo) => {
+		switch (e.target.value) {
+			case "all":
+				todo.style.display = "flex";
+				break;
+			case "completed":
+				if (todo.classList.contains("completed")) {
+					todo.style.display = "flex";
+				} else {
+					todo.style.display = "none";
+				}
+				break;
+			case "pending":
+				if (!todo.classList.contains("completed")) {
+					todo.style.display = "flex";
+				} else {
+					todo.style.display = "none";
+				}
+				break;
+		}
+	});
 }
