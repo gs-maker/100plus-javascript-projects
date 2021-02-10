8a48e71a97b584c41bcc67ba966c5d9ee3c1737b
gs-maker <gerald.ssali@gmail.com>
2021-02-10 09:25:33 +0300

    currency converter

--- 39 Exchange Rate Coverter/index.js
+++ 39 Exchange Rate Coverter/index.js
@@ -4,13 +4,23 @@ const amountOne = document.getElementById("amount-one");
 const currencyTwo = document.getElementById("currency-two");
 const amountTwo = document.getElementById("amount-two");
 
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
 // event listeners
 currencyOne.addEventListener("change", calculateCX);
 amountOne.addEventListener("input", calculateCX);
 currencyTwo.addEventListener("change", calculateCX);
 amountTwo.addEventListener("input", calculateCX);
 
-// functions
+swap.addEventListener("click", () => {
+	const temp = currencyOne.value;
+	currencyOne.value = currencyTwo.value;
+	currencyTwo.value = temp;
+	calculateCX();
+});
+
+// fetch exchange rates and update DOM
 function calculateCX() {
 	const currency_one = currencyOne.value;
 	const currency_two = currencyTwo.value;
@@ -19,6 +29,10 @@ function calculateCX() {
 		.then((response) => response.json())
 		.then((data) => {
 			console.log(data);
+			const rate = data.rates[currency_two];
+
+			rateEl.innerHTML = `1 ${currency_one} = ${rate} ${currency_two}`;
+			amountTwo.value = (amountOne.value * rate).toFixed(2);
 		});
 }
 
